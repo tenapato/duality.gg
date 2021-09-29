03c2017a6d2aa11c8f561d8b327c22491384a6ff
tenapato <tenapatricio@gmail.com>
2021-09-29 18:24:22 -0500

    Added loggin screen when not logged in

---
+++ Duality.gg/Controllers/ProfileViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ProfileViewController.swift
+//  Duality.gg
+//
+//  Created by user189106 on 9/29/21.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let userLoggedIn = true
+        if userLoggedIn {
+            notLoggedIn()
+            //let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+            //self.present(loginViewController, animated: true)
+            
+        }
+        
+    }
+    
+    
+    func notLoggedIn(){
+        //let loginScreen = LoginViewController()
+        //self.present(loginScreen, animated: true)
+        if let secondViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController {
+
+                    self.present(secondViewController, animated: true, completion: nil)
+                }
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
